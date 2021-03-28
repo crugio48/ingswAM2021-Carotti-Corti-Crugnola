53ec7d20b379fcb05371e646c80b1c39dc006510
andreacarotti <andrea1.carotti@mail.polimi.it>
2021-03-28 22:32:10 +0200

    commit locale

--- src/main/java/it/polimi/ingsw/model/resources/ResourceBox.java
+++ src/main/java/it/polimi/ingsw/model/resources/ResourceBox.java
@@ -37,6 +37,7 @@ public class ResourceBox {
         }
     }
 
+
     /**
      * this method is used in combination with a similar method in the storage by a controller that checks if the player has enough resources of any given type
      * @param resourceName is one of the personal fixed strings given to resources ("stones" or "shields"or ....)
