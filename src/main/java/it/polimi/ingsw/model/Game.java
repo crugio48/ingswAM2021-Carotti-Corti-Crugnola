b45520fe5412417ec329a0321e6abb0f740780e3
andreacarotti <andrea1.carotti@mail.polimi.it>
2021-04-16 15:37:46 +0200

    update javadoc

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -29,6 +29,8 @@ public class Game {
     private DevCardSpace devCardSpace;
     private MarbleContainer market;
 
+    //initialization of the game in the constructor:
+    //hardcoding of the action Cards instead of using json since there are just 7 cards
     public Game(int numOfPlayers) {
         this.numOfPlayers = numOfPlayers;
         this.currentPlayer = 1;
@@ -57,14 +59,19 @@ public class Game {
         try {
             Object leaderResourceObj = parser.parse(new FileReader("jsonFiles/LeaderCardsResource.json"));
             JSONObject leaderResourceJsonObj = (JSONObject) leaderResourceObj;
+            //creating the json Array that we will iterate in order to get the Leader Card
             JSONArray leaderResourceArrayCards = (JSONArray) leaderResourceJsonObj.get("cards");
             Iterator<JSONObject> leaderResourceIterator = leaderResourceArrayCards.iterator();
 
+            //iterating over the list
             while(leaderResourceIterator.hasNext()) {
                 tempCard = leaderResourceIterator.next();
 
                 int code = ((Long)tempCard.get("id")).intValue();
                 ResourceBox resRequirement = new ResourceBox();
+
+                //since these json Leader Cards are the ones that require resources to be activated
+                //We use a switch to distinguish among which is the required type of resource
                 switch((String)tempCard.get("required")) {
                     case("shields"):
                         resRequirement.addResource(new Shields(((Long)tempCard.get("quantity")).intValue()));
@@ -131,6 +138,8 @@ public class Game {
 
                 LeaderCard newCard = new LeaderCard(code,devRequirement,leaderEff,vicPoints);
 
+                //adding the leader Card just created to the hashMap
+                //the HashMap is used to easily call initialLeaderCardToPlayer
                 this.leaderCards.put(code,newCard);
             }
         } catch (ParseException e) {
@@ -143,7 +152,13 @@ public class Game {
 
     }
 
-
+    /**
+     * If the player already exists
+     * it also provides to the player the proper turn number
+     * @param p is the new player added to the game
+     * @return false ff the player already exists
+     * @return false and if the limit of player is already reached
+     */
     public boolean addPlayerToGame(Player p) {
         if (players.size() >= this.numOfPlayers) return false;
         if(this.getPlayerByNickname(p.getUsername()) != null) return false;
@@ -153,6 +168,14 @@ public class Game {
         return true;
     }
 
+    /**
+     *
+     * @param nickname is the string containing the nickname of the player
+     *                 we want to look for in the list of players already in the game
+     * @return the Player associated with that nickname if the nickname match a player already
+     * in the game
+     * @return null if the player doesn't exist
+     */
     public Player getPlayerByNickname(String nickname){
         for(Player p: players) {
             if (p.getUsername().equals(nickname)) return p;
@@ -160,6 +183,12 @@ public class Game {
         return null;   //return nullif player with that nickname doesn't exist in the game
     }
 
+    /**
+     * this method is used to get the actionCard from the actionCards' deck, add
+     * the action card to the other deck (the one containing only the used actionCards)
+     * and activating the effect of the action card by passing the entire Game
+     *
+     */
     public void drawAndExecuteActionCard(){
         ActionCard topCard = (ActionCard) actionCardDeck.getLastCardAndDiscard();
 
@@ -168,6 +197,12 @@ public class Game {
         topCard.activateEffect(this);
     }
 
+    /**
+     * method used to mix the actionCards, in order to do that we need to:
+     * - remove the every action Card from the stack of usedActionCards
+     * - adding the cards to the deck of the usable actionCards
+     * - shuffling this deck
+     */
     public void mixActionCards(){
         ActionCard temp;
         while (!usedActionCards.isEmpty()) {
@@ -177,6 +212,11 @@ public class Game {
         actionCardDeck.randomizeDeck();
     }
 
+
+    /**
+     * if the currentPlayer (that can be either 1, 2, 3 or 4 if the players in the game are 4)
+     * is 1, 2 or 3 we increment the value, if it's 4, becomes 1
+     */
     public void endTurn(){
         if (currentPlayer < numOfPlayers) currentPlayer++;
         else currentPlayer = 1;
@@ -214,8 +254,12 @@ public class Game {
         Player p = this.getPlayerByNickname(playerNickname);
         if (p == null) return false;
         if (p.getLeaderCard(1) != null || p.getLeaderCard(2) != null) return false;
+        //checking if in the leaderCards HashSet there is the code1, if not it means there is
+        //no card with that code and so we return false
         if (!leaderCards.containsKey(code1) || !leaderCards.containsKey(code2)) return false;
 
+        //removing from the hashset of leaderCards the card associated with the code1 and code2 and
+        //adding it to the slots of Leadercards of the Player
         p.setLeaderCard(leaderCards.remove(code1),1);
         p.setLeaderCard(leaderCards.remove(code2),2);
         return true;
