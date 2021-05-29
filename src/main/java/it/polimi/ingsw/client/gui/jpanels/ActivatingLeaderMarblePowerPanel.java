5c042aac58f324a47af13909cc80a09d91adc6ac
crugio48 <79754321+crugio48@users.noreply.github.com>
2021-05-29 16:04:51 +0200

    new panel skeleton

---
+++ src/main/java/it/polimi/ingsw/client/gui/jpanels/ActivatingLeaderMarblePowerPanel.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.client.gui.jpanels;
+
+import it.polimi.ingsw.client.gui.ClientGUI;
+
+import javax.swing.*;
+
+public class ActivatingLeaderMarblePowerPanel extends JPanel {
+
+
+
+
+
+    public ActivatingLeaderMarblePowerPanel(ClientGUI clientGUI, int jolly, int stones, int servants, int shields, int coins, String[] targetResources) {
+
+
+
+
+
+    }
+}
